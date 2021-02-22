e9426c94794501e6fc03fa97366e81bd9339439e
Jordi Vidaña <jordivid@gmail.com>
2021-02-22 18:32:00 +0100

    Càrrega inicial del projecte

---
+++ src/components/Hijo.vue
@@ -0,0 +1,14 @@
+<template>
+    <h2>{{mensaje}}</h2>
+</template>
+
+<script>
+export default {
+    name: 'Hijo',
+    data() {
+        return {
+            mensaje: "Soc el fill"
+        }
+    }
+}
+</script>
---
+++ src/components/Padre.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>{{mensaje}}</h2>
+        <Hijo></Hijo>
+    </div>
+</template>
+
+
+<script>
+import Hijo from './../components/Hijo.vue'; 
+
+export default {
+    name: 'Padre',
+    components: {
+        Hijo
+    },
+    data() {
+        return {
+            mensaje: "Soc el pare"
+        }
+    }
+}
+</script>
